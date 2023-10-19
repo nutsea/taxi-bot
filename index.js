58db0249af3812a39d253c8c0d8813bde71ee82b
Anastasia Batyrova <batyrova.n@icloud.com>
2023-10-19 18:34:57 +0300

    https settings

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const cors = require('cors')
 const router = require('./routes/index')
 const path = require('path')
+const https = require('https')
+const fs = require('fs')
 
 const PORT = process.env.PORT || 5000
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/bot.taxiallrussia.ru/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/bot.taxiallrussia.ru/cert.pem')
+}
 
 const app = express()
 app.use(cors())
@@ -11,9 +18,11 @@ app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router)
 
+const server = https.createServer(options, app)
+
 const start = async () => {
     try {
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
@@ -37,7 +46,7 @@ bot.on('text', (msg) => {
     const message = msg.text
 
     if (msg.text !== '/start' && msg.text !== '/help')
-    bot.sendMessage(chatId, `Неизвестная команда "${message}"`)
+        bot.sendMessage(chatId, `Неизвестная команда "${message}"`)
 })
 
 bot.onText(/\/help/, (msg) => {
